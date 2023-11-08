9ec54dcbc8bbc24a0188ddeabd0402b3ad5a8cc4
AHMADFADHILAH566 <149983833+AHMADFADHILAH566@users.noreply.github.com>
2023-11-08 14:55:51 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from "./componens/Navbar.jsx"
+function App() {
+  
+
+  return (
+    
+    <>
+    <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App
---
+++ src/componens/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar () {
+    return (
+        <div classname="ini navbar">
+        <h1> ini </h1> 
+        
+        <div>
+        <p>home</p>
+        <p>about</p>
+        <p>galery</p>
+        </div> 
+         </div>
+
+    );
+}
+export default Navbar
